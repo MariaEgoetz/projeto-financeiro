5ba8fd359e503ba57e90894b20256af71de0e9dd
MariaEgoetz <dudah2618@gmail.com>
2025-11-25 18:33:25 -0300

    teste deploy

--- start.sh
+++ start.sh
@@ -6,11 +6,9 @@ set -e
 echo "Rodando Migrations..."
 python manage.py migrate
 
-echo "Coletando arquivos estáticos..."
-python manage.py collectstatic --noinput
-
 echo "Iniciando Celery Worker em Background..."
 # O '&' no final faz ele rodar em segundo plano
+# Usamos --concurrency=1 para gastar pouca memória do servidor grátis
 celery -A core worker --loglevel=info --concurrency=1 &
 
 echo "Iniciando Gunicorn (Servidor Web)..."
